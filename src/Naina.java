f0702e18cd9b2cf0685344b025a88bdfa9690e30
gupta <nandinee.gupta_cs18@gla.ac.in>
2020-03-06 07:22:32 +0530

    in this question array size is not given so we take input in string

--- src/Naina.java
+++ src/Naina.java
@@ -5,7 +5,11 @@ public class Naina {
         Scanner sc = new Scanner(System.in);
         String str = sc.nextLine();
         String str2 = "";
+        int count = 0;
+        int p = 0;
+        for (int i = 0; i < str.length(); i++) {
 
 
+        }
     }
 }
