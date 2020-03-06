99d8bca9b18be09e523b97dab79f2db9fa3d8790
gupta <nandinee.gupta_cs18@gla.ac.in>
2020-03-06 07:19:11 +0530

    taking input from the Scanner

--- src/Naina.java
+++ src/Naina.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class Naina {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+
 
     }
 }
